6291c53519e081b283ea63e07ded35de81d3678e
Sam <swkane@indiana.edu>
2017-08-15 23:40:00 -0400

    Finished index

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const data = require('./data');
+const path = require('path');
+const mustacheExpress = require('mustache-express');
+const app = express();
+app.use(express.static("public"))
+
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.get("/", function(req, res){
+  res.render('index', data);
+});
+
+// TODO: set up a click event to redirect to the id of the result that was clicked
+app.get("/users/:id", function(req, res){
+  res.render('profile', data);
+});
+
+app.listen(3000, function(){
+  console.log("To infinity and beyond!");
+});
+
+// console.log(data);
+// console.log(data.users);
+// console.log(data.users[0]);
