1d484458a50cb8fc1e5ea124f06d2f4a74d54780
Sam <swkane@indiana.edu>
2017-08-29 15:41:13 -0400

    complete CRU functionality / authenticating users

--- app.js
+++ app.js
@@ -10,23 +10,26 @@ const bcrypt = require('bcrypt');
 const profileController = require('./controllers/profile');
 const unemployedController = require('./controllers/unemployed');
 const employedController = require('./controllers/employed');
-
-
+const session = require('express-session');
+// Boiler Plate
 // bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
-// Mustache Boiler Plate
+// Mustache
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
-
 // Setting up the style sheet
 app.use(express.static("public"));
-
-// Mongo Boiler Plate
+// Mongo
 // Connection Url
 var url = 'mongodb://localhost:27017/return-dir';
+// session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // !! Only run this once to set up mongo
 // Use connect method to conenct to the server
@@ -44,7 +47,9 @@ app.get("/", function(req, res){
       .then(function(data) {
         db.close();
         // console.log(data);
-        res.render('index', {users: data});
+        console.log(req.session.username);
+        console.log(data);
+        res.render('index', {users: data, currentUser: req.session.username});
       })
     })
 });
@@ -61,15 +66,15 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-  let username = req.body.username;
-  let password = req.body.password;
+  req.session.username = req.body.username;
+  req.session.password = req.body.password;
   MongoClient.connect(url)
   .then(function(db) {
     db.collection('users')
-    .findOne({username: username})
+    .findOne({username: req.session.username})
       .then(function (data) {
         db.close()
-        if (bcrypt.compareSync(password, data.passwordHash)) {
+        if (bcrypt.compareSync(req.session.password, data.passwordHash)) {
           res.redirect('/');
         } else {
           res.send('nope');
@@ -84,10 +89,10 @@ app.get('/create', function(req, res) {
 
 app.post('/create/user', function(req, res) {
   console.log("You tried to add a record!");
-  let password = bcrypt.hashSync(req.body.password, 8);
+  req.session.password = bcrypt.hashSync(req.body.password, 8);
   MongoClient.connect(url)
     .then(function(db) {
-      var robot = db.collection('users').insertOne({username: req.body.username, name: req.body.name, university: req.body.university, passwordHash: password})
+      var robot = db.collection('users').insertOne({username: req.body.username, name: req.body.name, email: req.body.email, job: req.body.job, company: req.body.company, skills: req.body.skills, university: req.body.university, phone: req.body.phone, address: req.body.address, passwordHash: req.session.password})
       .then(function(user) {
         console.log(user);
       });
@@ -104,6 +109,37 @@ app.post('/create/user', function(req, res) {
   //     console.log("User Added Record Error");
   //     // action to take on error
   //   });
+  res.redirect('/');
+});
+
+app.get('/logout', function(req, res) {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+app.get('/edit', function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+    db.collection('users').findOne({username: req.session.username})
+    .then(function(docs) {
+      console.log(docs);
+      res.render('edit', docs);
+    });
+    db.close();
+  })
+});
+
+app.post('/edit/complete', function(req, res) {
+  console.log("You tried to add a record!");
+  req.session.password = bcrypt.hashSync(req.body.password, 8);
+  MongoClient.connect(url)
+    .then(function(db) {
+      var robot = db.collection('users').updateOne({username: req.session.username}, {$set: {username: req.body.username, name: req.body.name, email: req.body.email, job: req.body.job, company: req.body.company, skills: req.body.skills, university: req.body.university, phone: req.body.phone, address: req.body.address, passwordHash: req.session.password}})
+      .then(function(user) {
+        console.log(user);
+      });
+      // console.log(db.users.findOne({username: req.body.username}));
+      db.close();
+  });
   res.redirect('/');
 });
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+2 Things to Build Out
+
+-- Ability to edit user entry
+
+-- Take away login and register buttons if someone is logged in, and replace them with an edit button which will take the user to a screen where they can edit their own profile
